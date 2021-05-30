486737ae44feb2ae6f75b60183323da608700f42
Lim223 <shfks1212@naver.com>
2021-05-30 16:27:10 +0900

    audioAdd

--- carrot.js
+++ carrot.js
@@ -4,7 +4,10 @@ const gamePoint = document.querySelector('.Point');
 const modalBox = document.querySelector('.modalBox');
 const playGround = document.querySelector('.playGround');
 const itemImg = document.querySelectorAll('.itemImg');
-
+const audioBg = document.querySelector('#audioBg')
+const audioCarrot = document.querySelector('#audioCarrot')
+const audioWin = document.querySelector('#audioWin')
+const audioLose = document.querySelector('#audioLose')
 
 let time = 10;
 let point = 10;
@@ -17,7 +20,40 @@ let playing = false;
 const SHOWINGModal = 'showingModal';
 const SHOWINGImg = 'showingImg'
 
+/*오디오 설정*/
+function loadAudio(){
+  audioBg.load();
+  audioBg.volume = 0.2;
+  if(playing){
+  audioCarrot.autoplay = true;
+  } else if (!playing){
+    audioBg.pause();
+  }
+}
 
+function audioCarrotBg(e){
+  if(playing && e.target.classList.contains('carrotimg')){
+  audioCarrot.autoplay = true;
+  audioCarrot.load();
+  audioCarrot.volume = 0.5;
+  }
+}
+
+function audioWinBg(){
+  audioWin.autoplay = true;
+  audioWin.load();
+  audioWin.volume = 0.5;
+}
+
+function audioLoseBg(e){
+  if(playing && e.target.classList.contains('bugimg')){
+    audioLose.load();
+    audioLose.autoplay = true;
+    audioLose.volume = 0.5;
+  }
+}
+
+/*재시작 모달*/
 function reStartModal(){
   if(time === 0 || time > 0 && point > 0){
     modalBox.innerHTML =`
@@ -33,31 +69,37 @@ function reStartModal(){
       <button class="reStart" type="button" name="button">again!</button>
     </div>
     `
+    audioWinBg();
     }
   modalBox.classList.add(SHOWINGModal);
-  console.log(modalBox);
 }
 
+/*게임 stop*/
 function stopGame(){
   playing = false;
+  loadAudio();
   clearInterval(timeInterval);
   startBtn.disabled = true;
   startBtn.textContent = "END";
   reStartModal();
 }
 
+/*시간 카운트*/
 function timeCount(){
   startBtn.textContent = "STOP";
   startBtn.disabled = false;
   if(time <= timeset){
+    playing = true;
     time--;
     timeBox.innerText = `00:${time >= 10 ? `${time}` : `0${time}`}`
   }
   if(time <= 0){
+    playing = false;
     stopGame()
   }
 }
 
+/*랜덤한 위치에 당근과 벌레 display*/
 function showCarrot(){
   let playRect = playGround.getBoundingClientRect();
   let playRectWidth = playRect.width;
@@ -72,6 +114,7 @@ function showCarrot(){
   })
 }
 
+/*당근 선택시 당근 hidden*/
 function removeCarrot(e){
   if(playing && e.target.classList.contains('carrotimg')){
       e.target.style.visibility = "hidden"
@@ -83,17 +126,20 @@ function removeCarrot(e){
     }
 }
 
+/*버그 클릭 시 게임 stop*/
 function bugClick(e){
   if(playing && e.target.classList.contains('bugimg')){
     stopGame();
   }
 }
 
+/*게임 시작*/
 function playingStart(){
   if(playing){
     return;
   }
   playing = true;
+  loadAudio();
   time = timeset;
   point = ponitset;
   timeBox.innerText = `00:${time >= 10 ? `${time}` : `0${time}`}`;
@@ -103,18 +149,25 @@ function playingStart(){
   showCarrot();
 }
 
+/*클릭 이벤트 설정&*/
+startBtn.addEventListener('click',() => {
+  playingStart();
+});
+playGround.addEventListener('click',(e) => {
+  audioCarrotBg(e);
+  audioLoseBg(e);
+  removeCarrot(e);
+  bugClick(e);
+});
+modalBox.addEventListener('click',(e) =>
+  {e.target.tagName = 'BUTTON' && playingStart();
+  }
+)
+
+/*최초 로딩시 시간, 포인트 설정*/
 function init(){
   timeBox.innerText = startTime;
   gamePoint.innerText = startPoint;
 }
-
-startBtn.addEventListener('click',playingStart);
-playGround.addEventListener('click',(e) => removeCarrot(e));
-playGround.addEventListener('click',(e) => bugClick(e));
-modalBox.addEventListener('click',(e) =>
-  {e.target.tagName = 'BUTTON' && playingStart();
-  console.log(modalBox)
-  }
-)
 
 init()
